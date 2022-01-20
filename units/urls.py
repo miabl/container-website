11925877bfc0695e7a0047808bc446aae7638881
emmapaterson1 <88269698+emmapaterson1@users.noreply.github.com>
2022-01-20 22:51:44 +0800

    added view - units

--- units/urls.py
+++ units/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('', views.UnitList.as_view(), name="unit-list"),
 ]
--- units/views.py
+++ units/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
+from .models import Unit
 
-# Create your views here.
+class UnitList(LoginRequiredMixin, generic.ListView):
+    login_url = '/login/'
+    model = Unit
+    context_object_name = 'unit_list'
+    template_name = 'units/unit-list.html'
