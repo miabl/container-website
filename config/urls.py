0314939ac3687309bf6e0e15aaae2b8fb8c47931
emmapaterson1 <88269698+emmapaterson1@users.noreply.github.com>
2022-01-20 16:34:54 +0800

    config\urls.py - added units urls

--- config/urls.py
+++ config/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('containers/', include('containers.urls')),
+                  path('units/', include('units.urls')),
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
