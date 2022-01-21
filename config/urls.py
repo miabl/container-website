e455a89bfb5964fd483fdc1ce088998a92595d6a
t4junkk <91582121+t4junkk@users.noreply.github.com>
2022-01-21 15:33:53 +0800

    registration url

--- config/urls.py
+++ config/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
 from django.conf.urls.static import static
+from students import views
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('containers/', include('containers.urls')),
                   path('units/', include('units.urls')),
                   path('accounts/', include('django.contrib.auth.urls')),
+                  path('register/', views.register, name="register"),
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
