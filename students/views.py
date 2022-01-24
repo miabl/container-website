8a37e51552cbb60bfce82eb229d513a627048d96
t4junkk <91582121+t4junkk@users.noreply.github.com>
2022-01-24 12:46:13 +0800

    update redirect to home

--- students/views.py
+++ students/views.py
@@ -9,7 +9,7 @@ def register(response):
         if form.is_valid():
             form.save()
 
-        return redirect('/')
+        return redirect('index')
     else:
         form = NewUserForm()
 
