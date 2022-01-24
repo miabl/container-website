422b9361834af9285b98afb2ea244639300f67b3
Mia <80946195+miabl@users.noreply.github.com>
2022-01-24 14:31:33 +0800

    Added running_container field to student model

--- students/models.py
+++ students/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 from units.models import Unit
+from containers.models import ContainerInstance
 
 
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     units = models.ManyToManyField('units.Unit')
-
-# Create your models here.
+    running_container = models.OneToOneField('containers.ContainerInstance', on_delete=models.SET_NULL, null=True,
+                                             blank=True)
